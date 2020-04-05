bcb8ea8e5d05094378a197ab2360f4ecb2cf6d74
sachin sac <sachin.andro12@gmail.com>
2020-04-05 14:49:13 -0700

    Added vhd files

--- ExU/SourceCode/Adder.vhd
+++ ExU/SourceCode/Adder.vhd
@@ -1,112 +1,68 @@
------------------------------------------------------------------------------
--- Declare the Input network that produces the Generate and Propagate signals
------------------------------------------------------------------------------
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity GPnet is
-  generic ( width : integer := 16 );
-  port (
-    X, Y     :     in     std_logic_vector(width-1 downto 0);
-    G, P     :     out    std_logic_vector(width-1 downto 0) );
-end entity GPnet;
 
-architecture Structural of GPnet is
+ library ieee; 
+ use ieee.std_logic_1164.all;  
+ 
+ entity Adder is  
+ Generic ( N : natural := 64 );
+	port( 
+	  A, B : in std_logic_vector(63 downto 0 );  
+	  Y : out std_logic_vector(63 downto 0 );
+	  
+	  Cin : in std_logic;
+	  Cout : out std_logic;
+	  Ovfl : out std_logic
+	  );
+ end entity Adder;  
+ 
+architecture rtl of Adder is
+	
+	signal s, h: std_logic_vector(N-1 downto 0);
+	signal carryIn : std_logic_vector(N downto 0);
+	
 begin
-  bitcell: for i in width-1 downto 0
-  generate
-  begin
-    -- cellGi: entity Work.and2 port map ( X(i), Y(i), G(i) );
-    -- cellPi: entity Work.xor2 port map ( X(i), Y(i), P(i) );
-    G(i) <= X(i) and Y(i);
-    P(i) <= X(i) xor Y(i);
-  end generate bitcell;
-end architecture Structural;
+	carryIn(0) <= Cin;
 
------------------------------------------------------------------------------
--- Declare the Output network that produces the Sum bits. The output carry is
--- Labeled as an additional Sum Bit.
------------------------------------------------------------------------------
+	
+adderLoop:	for i in 0 to N-1 generate
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+	s(i) <= (A(i) XOR B(i)) XOR carryIn(i);
+	carryIn(i+1) <= ( ( A(i) XOR B(i) ) AND carryIn(i) ) OR ( A(i) AND B(i) );
 
-entity Snet is
-  generic ( width : integer := 16 );
-  port (
-    P        :     in     std_logic_vector(width-1 downto 0);
-    C        :     in     std_logic_vector(width downto 0);
-    S        :     out    std_logic_vector(width downto 0) );
-end entity Snet;
+	Y(i) <= s(i);
+	end generate adderLoop;
+	
+	Cout <= carryIn(N);
+	Ovfl <= carryIn(N-1) XOR carryIn(N);
+	
+end architecture rtl;
 
-architecture Structural of Snet is
-begin
-  
-  bitcell: for i in width-1 downto 0
-  generate
-  begin
-    -- cellSi: entity Work.xor2 port map ( P(i), C(i), S(i) );
-    S(i) <= P(i) xor C(i);
-  end generate bitcell;
-  
-  S(width) <= C(width);
-  
-end architecture Structural;
+ -- http://www.c-jump.com/CIS77/CPU/Overflow/lecture.html#O01_0080_overfow_output - Overflow.
 
------------------------------------------------------------------------------
--- Declaration of an adder. The adder contains 3 entities. Different
--- architectures differ only in their Carry network. The adder can be used to
--- instantiate circuits of varying width. The default is 16-bits.
------------------------------------------------------------------------------
+-- architecture rtl of Adder is  
+ 
+	-- signal P, G: std_logic_vector(63 downto 0 );
+	-- signal temp: std_logic;
+	
+ -- begin    
+	-- temp <= Cin;
+	-- Adder_loop: for i in 0 to 63	generate
+	
+	   -- P(i) <= A(i) xor B(i);  
+	   -- G(i) <= A(i) and B(i);
+	   -- Cout <= G(i) or (P(i) and temp);  
+	   -- Y(i) <= P(i) xor temp;
+	   
+	   -- ifCond: if i > 1 generate
+		-- Ovfl <= temp xor (G(i) or (P(i) and temp));
+	   -- end generate ifCond;
+	   
+	   -- temp <= G(i) or (P(i) and temp);	-- temp <= Cout   
 
-library ieee;
-Use ieee.std_logic_1164.all;
-Use std.TEXTIO.all;
-Use ieee.numeric_std.all;
+	-- end generate Adder_loop;
+	
+	-- -- Ovfl <= temp xor (G(i-1) or (P(i-1) and temp));
+	
+-- end architecture rtl;
 
-entity Adder is
-  generic ( width : integer := 64 );
-  port (
-    X, Y     :     in     std_logic_vector(width-1 downto 0);
-    Cin      :     in     std_logic;
-    S        :     out    std_logic_vector(width downto 0) );
-end entity Adder;
-  
------------------------------------------------------------------------------
--- Declare the Carry network for the adder.
------------------------------------------------------------------------------
 
-library ieee;
-Use ieee.std_logic_1164.all;
-Use std.TEXTIO.all;
-Use ieee.numeric_std.all;
-
-entity Cnet is
-  generic ( width : integer := 16 );
-  port(
-    G, P     :     in     std_logic_vector(width-1 downto 0);
-    Cin      :     in     std_logic;
-    C        :     out    std_logic_vector(width downto 0));
-end entity Cnet;
-
------------------------------------------------------------------------------
--- Students must Create the following Carry Network Architectures.
------------------------------------------------------------------------------
-architecture Ripple of Cnet is
-  signal interC  : std_logic_vector(width downto 0); -- Don't read from output signal C
-  signal tempWire   : std_logic_vector(width-1 downto 0);
-begin
-  interC(0) <= Cin;
-
-	CpropGenerate: for index in 0 to width-1 generate
-    -- Cpropi: entity Work.Cprop port map(G(index), P(index), interC(index), interWire(index + 1));
-    tempWire(index) <= interC(index) and P(index);
-    interC(index + 1) <= G(index) or tempWire(index);
-  end generate CpropGenerate;
-  
-	C(width downto 0) <= interC(width downto 0);
-end architecture Ripple;
